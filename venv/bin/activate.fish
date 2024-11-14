95128a65f50addce38d96438fe760ee907626156
DonatelloDonini <donatellodonini05@gmail.com>
2024-11-14 08:26:35 +0100

    Reset virtual environment

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/donnynja/task_1/compare_trajectories/venv"
+set -gx VIRTUAL_ENV "/home/donnynja/compare_trajectories/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
